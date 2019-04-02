97ff8680e79539e6c93c00470ad0d45f4df4ce5e
jaffarsadique <jaffy71@gmail.com>
2019-04-02 19:06:23 +0530

    Group has been created

---
+++ src/group/Friends.java
@@ -0,0 +1,15 @@
+package group;
+
+public class Friends {
+		public void fixed(){
+			System.out.println("66");
+		}
+		public void saving(int ds){
+			System.out.println("99");
+		}
+		public static void main(String[] args) {
+			Friends i=new Friends();
+			i.fixed();
+			i.saving(6);
+			}}
+
